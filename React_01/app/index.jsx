0570070420a42663b1767508e071adfe7db109e5
niangao-warren <503809815@qq.com>
2018-01-08 17:56:30 +0800

    update

--- React_01/app/index.jsx
+++ React_01/app/index.jsx
@@ -3,14 +3,62 @@ import { render } from 'react-dom'
 
 import './static/css/common.less'
 
+// 定义组件
 class Hello extends React.Component {
-    render() {
+    // jsx 返回多个节点
+    // render(){
+    //     return ( 
+    //         <div>
+    //             {/* 注释 */}
+    //             <p className="n1">Nian糕_1</p>
+    //             <p style={{color:'#ccc',fontSize:'20px'}}>Nian糕_2</p>
+    //             <p>Nian糕_3</p> 
+    //         </div> 
+    //     )
+    // }
+
+    // jsx 事件
+    // render(){
+    //     return (
+    //         <p onClick={this.clickText.bind(this)}>Nian糕</p>
+    //     )
+    // }
+    // clickText(e) {
+    //     e.preventDefault();
+    //     console.log(this);
+    // }
+
+
+    // jsx 循环
+    // render() {
+    //     const arr = ['Nian糕_1', 'Nian糕_2', 'Nian糕_3']
+    //     return (
+    //         <div>
+    //             <ul>
+    //                 {arr.map((item, index) => {
+    //                     return <li key={index}>this is {item}</li>
+    //                 })}
+    //             </ul>
+    //         </div>
+    //     )
+    // }
+
+    // jsx 判断
+    render(){
         return (
-            <p>Hello World</p>
+            <div>
+                {
+                    true
+                    ? <p>Nian糕_true</p>
+                    :<p>Nian糕_false</p>
+                }
+            </div>
         )
     }
+    
 }
 
+// 渲染组件到页面
 render(
     <Hello/>,
     document.getElementById('root')
