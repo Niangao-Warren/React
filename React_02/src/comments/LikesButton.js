4c722178194f967cd9be12be445a22a799397f2e
Niangao-Warren <503809815@qq.com>
2020-04-21 14:56:06 +0800

    update

--- React_02/src/comments/LikesButton.js
+++ React_02/src/comments/LikesButton.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 // 函数型组件
 const LikeButton = () => {
-  // 当前的state，更新state的函数
   const [ like, setLike ] = useState(0)
-  console.log(useState(0))
+  
+  useEffect(() => {
+    document.title = `You clicked ${like} times`;
+  })
+  
   return (
   <button onClick={() => {setLike(like + 1)}}>
     喜欢数 {like}
